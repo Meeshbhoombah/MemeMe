c99e52c7627fba9901c0828b2567d2ea71373ca3
Rohan Mishra <rohanmishra2017@gmail.com>
2016-08-09 15:02:01 -0400

    New version

---
+++ MemeMe/MemeMe/MemeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MemeCollectionViewCell.swift
+//  Meme
+
+import Foundation
+import UIKit
+
+// Collection cell class used to hold the memed image
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var memeImageView: UIImageView!
+    
+}
---
+++ MemeMe/MemeMe/MemeTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MemeTableViewCell.swift
+//  MemeMe
+//
+
+
+import Foundation
+import UIKit
+
+// Custom table view cell that represents a sent meme
+class MemeTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var memeImageView: UIImageView!
+    @IBOutlet weak var memeLabel: UILabel!
+    
+}
